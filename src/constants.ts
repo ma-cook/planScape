d63eebd63fd39599ce4f50d3437574f3196a4345
Mitchell <mitchell.cook@outlook.co.nz>
2026-04-13 06:01:00 +1200

    oauth id added

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 export const FIREBASE_API_KEY = "AIzaSyAfKQkefaPgldmnXaO1pNLYczBjudoGbn8";
 export const FIREBASE_PROJECT_ID = "hoverchart";
+export const GOOGLE_OAUTH_CLIENT_ID = "52303258436-h7sknvnqaicl03div2cthv4dqhuopm1s.apps.googleusercontent.com"
+export const BULK_IMPORT_URL = "https://bulkimport-qtk2xsi74a-uc.a.run.app";
+
